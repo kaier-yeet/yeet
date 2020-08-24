4e3462640585a00f596e4afabf106b5754f29b6e
kaier-yeet <70123543+kaier-yeet@users.noreply.github.com>
2020-08-24 16:40:31 +0800

    Add files via upload

---
+++ scorce for final lol.py
@@ -0,0 +1,14 @@
+score=int(input("plz input score:"))
+if score>=89:
+    print('A')
+elif score>=83:
+    print('B')
+elif score>=72:
+    print('C')
+elif score>=61:
+    print('D')
+elif score>=50:
+    print('E')
+else :
+    print('FFFFFFF')
+    
---
+++ score 3.py
@@ -0,0 +1,8 @@
+eng=int(input("plz input eng score:"))
+math=int(input("plz input math score:"))
+if eng>=90 and math>=90:
+    print('prizes guranteed')
+elif eng<=60 or math<=60:
+    print('punishment')
+elif eng<=60 or math<=60:
+    print('get better')
